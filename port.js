2c8df3e18910af66367ce891a29ede888799c388
Adam SOudani <asoudani@email.com>
2020-04-06 14:31:29 -0700

    added js to link github projects

---
+++ port.js
@@ -0,0 +1,21 @@
+$('#darts').on('click', function(){
+    window.location = 'https://adoni0.github.io/GIPHY-API-Project/';
+    
+});
+
+$('#city').on('click', function(){
+    window.location = 'https://adoni0.github.io/Train-Scheduler/';
+    
+});
+
+$('#rubix').on('click', function(){
+    window.location = 'https://adoni0.github.io/Unit-4-game/';
+    
+});
+
+$('#gaming').on('click', function(){
+    window.location = 'https://github.com/KittM/project-1';
+    
+});
+
+
